c4d649bd562db597e56dcf847d700c1ad89a56ea
sonique <sonique6784@yahoo.fr>
2022-01-18 18:52:36 +1100

    step 4 - added food and obstacles

--- common/src/commonMain/kotlin/me/sonique/common/leotales/game/LeoTales.kt
+++ common/src/commonMain/kotlin/me/sonique/common/leotales/game/LeoTales.kt
@@ -6,6 +6,7 @@ import me.sonique.common.core.CGDKGame
 import me.sonique.common.Placement
 import androidx.compose.ui.unit.dp
 import toVector2
+import rightShift
 
 // 480 width, 320 height 
 val WINDOW_SIZE = IntSize(480, 320)
@@ -19,10 +20,20 @@ class LeoTales : CGDKGame() {
     val energy = mutableStateOf(1)
 
     val leo = Leo()
+    val food = Food()
+    val obstacle = Obstacle()
+
+
     val placementHelper = Placement(GAME_SIZE.toVector2())
 
     init {
         leo.mutablePosition.value = placementHelper.toBottomCenter(leo)
         this.addGObject(leo)
+
+        food.mutablePosition.value = placementHelper.toBottomCenter(food).rightShift(170)
+        this.addGObject(food)
+
+        obstacle.mutablePosition.value = placementHelper.toBottomCenter(obstacle).rightShift(100)
+        this.addGObject(obstacle)
     }
 }
---
+++ common/src/commonMain/kotlin/me/sonique/common/leotales/game/Obstacle.kt
@@ -0,0 +1,6 @@
+package me.sonique.common.leotales.game
+
+import me.sonique.common.core.ImageCGDKObject
+import org.openrndr.math.Vector2
+
+class Obstacle: ImageCGDKObject("obstacle.png", size=Vector2(25.0, 50.0))
